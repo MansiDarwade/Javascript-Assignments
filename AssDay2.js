3a18216b91287add5d80d12e6913a26bbf2a77d9
MansiDarwade <77888467+MansiDarwade@users.noreply.github.com>
2021-01-23 20:12:43 +0530

    Add files via upload

---
+++ AssDay2.js
@@ -0,0 +1,5 @@
+var arr=[23,56,78,90,34] var sum=0; for(var i=0;i
+<=4;i++) { sum=sum+arr[i]; } console.log(sum);
+<!-- Visual Code Mobile Developed By Manish Nirmal App Available on Play Store
+:- https://play.google.com/store/apps/details?id=lk.visual.code.mobile YouTube
+:- https://youtube.com/ManishNirmal -->
