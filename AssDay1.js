baa14a0dc7331055d36c780a1b7d543587b173f3
MansiDarwade <77888467+MansiDarwade@users.noreply.github.com>
2021-01-23 19:58:44 +0530

    Add files via upload

---
+++ AssDay1.js
@@ -0,0 +1,17 @@
+<!--
+Visual Code Mobile
+Developed By Manish Nirmal
+App Available on Play Store :-
+https://play.google.com/store/apps/details?id=lk.visual.code.mobile
+YouTube :-
+https://youtube.com/ManishNirmal
+-->
+var marks=80; if(marks>=90) { console.log("AA"); } else if(mark>=80&&marks
+<=90)
+{ console.log( "AB"); } else if(mark>=70&&marks
+  <=80) { console.log( "BB"); } else if(mark>=60&&marks
+    <=70) { console.log( "BC"); } else if(mark>=50&&marks
+      <=60) { console.log( "CC"); } else if(mark>=40&&marks
+        <=50) { console.log( "CD"); } else if(mark>=30&&marks
+          <=40) { console.log( "DD"); } else(marks<=30) { console.log(
+          "FF"); }
