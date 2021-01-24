b9b91a7d9fda5fcb8abf8154809219208313ce86
MansiDarwade <77888467+MansiDarwade@users.noreply.github.com>
2021-01-24 20:31:27 +0530

    Update AssDay1.js

--- AssDay1.js
+++ AssDay1.js
@@ -1,17 +1,33 @@
-<!--
-Visual Code Mobile
-Developed By Manish Nirmal
-App Available on Play Store :-
-https://play.google.com/store/apps/details?id=lk.visual.code.mobile
-YouTube :-
-https://youtube.com/ManishNirmal
--->
-var marks=80; if(marks>=90) { console.log("AA"); } else if(mark>=80&&marks
-<=90)
-{ console.log( "AB"); } else if(mark>=70&&marks
-  <=80) { console.log( "BB"); } else if(mark>=60&&marks
-    <=70) { console.log( "BC"); } else if(mark>=50&&marks
-      <=60) { console.log( "CC"); } else if(mark>=40&&marks
-        <=50) { console.log( "CD"); } else if(mark>=30&&marks
-          <=40) { console.log( "DD"); } else(marks<=30) { console.log(
-          "FF"); }
+var marks=55; 
+if(marks>=90) 
+{
+     console.log("AA"); 
+} 
+else if(marks>=80&&marks<=90)
+{ 
+    console.log( "AB"); 
+} 
+else if(marks>=70&&marks<=80) 
+{ 
+    console.log( "BB"); 
+} 
+else if(marks>=60&&marks<=70) 
+    { 
+        console.log( "BC"); 
+    } 
+    else if(marks>=50&&marks<=60)
+     {
+         console.log( "CC"); 
+        }
+         else if(marks>=40&&marks<=50) 
+         { 
+             console.log( "CD");
+             } 
+             else if(marks>=30&&marks<=40) 
+             { 
+                 console.log("DD");
+                 } 
+                 else if(marks<=30)
+                  { 
+                      console.log("FF"); 
+                }
